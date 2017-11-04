54b3ff55272d8a1c1211c47ba90fa2c5d02636ae
Ger Hobbelt <ger@hobbelt.com>
2017-11-04 14:21:45 +0100

    updated NPM packages & tweaks to correct for us using scoped packages.

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@
 
 
 var path     = require('path');
-var generate = require('markdown-it-testgen');
+var generate = require('@gerhobbelt/markdown-it-testgen');
 
 /*eslint-env mocha*/
 
 describe('markdown-it-abbr', function () {
-  var md = require('markdown-it')({ linkify: true })
+  var md = require('@gerhobbelt/markdown-it')({ linkify: true })
               .use(require('../'));
 
   generate(path.join(__dirname, 'fixtures/abbr.txt'), md);
